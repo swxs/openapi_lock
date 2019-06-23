0004387d1a87389c7f6ff14bb1d924fec4ef44f6
swxs <466565029@qq.com>
2019-06-23 21:56:27 +0800

    no message

--- src/api/PasswordLock.js
+++ src/api/PasswordLock.js
@@ -1,18 +1,18 @@
-import request from './request.js'
-
 const PasswordLockUrl = '/api/password_lock/password_lock/'
 
 export const searchPasswordLock = (data) => {
   return new Promise(function(resolve, reject) {
-    request.get(`${PasswordLockUrl}`, {params: data}).then((response) => {
-      resolve(response)
-    })
+    window.axios
+      .get(`${PasswordLockUrl}`, { params: data })
+      .then((response) => {
+        resolve(response)
+      })
   })
 }
 
 export const selectPasswordLock = (id) => {
   return new Promise(function(resolve, reject) {
-    request.get(`${PasswordLockUrl}${id}/`).then((response) => {
+    window.axios.request.get(`${PasswordLockUrl}${id}/`).then((response) => {
       resolve(response.data)
     })
   })
@@ -20,7 +20,7 @@ export const selectPasswordLock = (id) => {
 
 export const createPasswordLock = (data) => {
   return new Promise(function(resolve, reject) {
-    request.post(`${PasswordLockUrl}`, data).then((response) => {
+    window.axios.request.post(`${PasswordLockUrl}`, data).then((response) => {
       resolve(response.data)
     })
   })
@@ -28,31 +28,37 @@ export const createPasswordLock = (data) => {
 
 export const copyPasswordLock = (id, data) => {
   return new Promise(function(resolve, reject) {
-    request.post(`${PasswordLockUrl}${id}/`, data).then((response) => {
-      resolve(response.data)
-    })
+    window.axios.request
+      .post(`${PasswordLockUrl}${id}/`, data)
+      .then((response) => {
+        resolve(response.data)
+      })
   })
 }
 
 export const updatePasswordLock = (id, data) => {
   return new Promise(function(resolve, reject) {
-    request.put(`${PasswordLockUrl}${id}/`, data).then((response) => {
-      resolve(response.data)
-    })
+    window.axios.request
+      .put(`${PasswordLockUrl}${id}/`, data)
+      .then((response) => {
+        resolve(response.data)
+      })
   })
 }
 
 export const modifyPasswordLock = (id, data) => {
   return new Promise(function(resolve, reject) {
-    request.patch(`${PasswordLockUrl}${id}/`, data).then((response) => {
-      resolve(response.data)
-    })
+    window.axios.request
+      .patch(`${PasswordLockUrl}${id}/`, data)
+      .then((response) => {
+        resolve(response.data)
+      })
   })
 }
 
 export const deletePasswordLock = (id) => {
   return new Promise(function(resolve, reject) {
-    request.delete(`${PasswordLockUrl}${id}/`).then((response) => {
+    window.axios.request.delete(`${PasswordLockUrl}${id}/`).then((response) => {
       resolve(response.data)
     })
   })
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+const token =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NTg1OTY2MjcsImlhdCI6MTU1ODU4OTQyNywiaXNzIjoiSURZOk1YIiwibmJmIjoxNTU4NTg5NDI3LCJvcmdfY29kZSI6ImlkeSIsIm9yZ19uYW1lIjoiXHU4MmNmXHU1ZGRlXHU0ZjE3XHU4YTAwXHU3ZjUxXHU3ZWRjXHU3OWQxXHU2MjgwXHU4MGExXHU0ZWZkXHU2NzA5XHU5NjUwXHU1MTZjXHU1M2Y4IiwidWlkIjoiNWNlMzZjZTZhNmM0OTBmY2UzMDA4MDc3IiwidW5hbWUiOiJcdThkODVcdTdlYTdcdTdiYTFcdTc0MDZcdTU0NTgiLCJhdmF0YXIiOm51bGwsInN1cGVyIjoxfQ.z6ikNmH8U3TB5XFNLtJxWo0l79_JnA-jctdabd6GsPE'
+window.token = token
+
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
@@ -17,24 +21,39 @@ let config = {
 const _axios = axios.create(config)
 
 _axios.interceptors.request.use(
-  function(config) {
-    // Do something before request is sent
+  (config) => {
+    // do something before request is sent
+    const tem = config.url.indexOf('?') !== -1 ? '&' : '?'
+
+    if (token) {
+      config.headers = {
+        Authorization: 'Bearer ' + token
+      }
+    }
+
+    if (config.method === 'get') {
+      config.url += tem + 'time_stamp=' + new Date().getTime()
+    }
+
     return config
   },
-  function(error) {
-    // Do something with request error
+  (error) => {
+    console.log(error)
     return Promise.reject(error)
   }
 )
 
 // Add a response interceptor
 _axios.interceptors.response.use(
-  function(response) {
-    // Do something with response data
-    return response
+  (data) => {
+    if (data.data.code === 0) {
+      return data.data.data
+    } else {
+      return Promise.reject(data.data.code)
+    }
   },
-  function(error) {
-    // Do something with response error
+  (error) => {
+    console.log(error)
     return Promise.reject(error)
   }
 )
